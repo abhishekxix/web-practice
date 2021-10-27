f0f3c07d05c4755ed6a62c320e520eb721ff93b3
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-10-27 17:29:07 +0530

    arrays

---
+++ app.js
@@ -0,0 +1,23 @@
+const btn = document.querySelector('.btn')
+btn.addEventListener('click', () => {
+  console.log('button clicked')
+})
+// console.log(btn)
+// for (let i = 0; i < 20; i++) {
+//   btn.click()
+// }
+// console.log(typeof undefined)
+const names = [
+  'abhishek',
+  'john',
+  'susan',
+  'peter',
+  'lmao',
+  'no',
+  'more',
+  'people',
+]
+
+names.forEach((item) => {
+  console.log(item)
+})
