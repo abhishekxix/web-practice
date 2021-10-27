cc18fab9b94d8a56e9b6dc89c96371a4af83df89
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-10-28 00:02:55 +0530

    event propagation

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict'
+/* 'use strict'
 
 const btn = document.querySelector('.btn')
 btn.addEventListener('click', () => {
@@ -53,3 +53,25 @@ second.innerHTML = first.innerHTML
 document.body.append(second)
 // second.remove()
 // document.body.removeChild(first)
+ */
+
+const btn = document.querySelector('.btn')
+const container = document.querySelector('.container')
+// const heading = document.querySelector('.heading')
+
+function sayHello() {
+  console.log('hello there')
+}
+
+// heading.addEventListener('click', sayHello)
+
+btn.addEventListener('click', function () {
+  const element = document.createElement('h1')
+  element.classList.add('heading')
+  element.textContent = `I am inside container`
+  container.appendChild(element)
+})
+
+container.addEventListener('click', function (e) {
+  if (e.target.classList.contains('heading')) console.log('hello there')
+})
