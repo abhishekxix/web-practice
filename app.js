b11f9dc92aa53e0fd5327c5f04802da726a7dfef
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-10-29 00:07:24 +0530

    XHR

--- app.js
+++ app.js
@@ -7,14 +7,23 @@ btn.addEventListener('click', () => {
 function getData() {
   const xhr = new XMLHttpRequest();
 
-  xhr.open('GET', './api/sample.txt');
+  xhr.open('GET', './api/sample.json');
 
   xhr.onreadystatechange = function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
-      console.log(xhr.responseText);
-      const text = document.createElement('p');
-      text.textContent = xhr.responseText;
-      document.body.appendChild(text);
+      // const text = document.createElement('p');
+      // text.textContent = xhr.responseText;
+      // document.body.appendChild(text);
+      console.log(typeof xhr.responseText);
+      const data = JSON.parse(xhr.responseText);
+      const displayData = data
+        .map((item) => {
+          return `<p> ${item.name}</p>`;
+        })
+        .join('');
+      const elt = document.createElement('div');
+      elt.innerHTML = displayData;
+      document.body.appendChild(elt);
     } else {
       console.log({
         status: xhr.status,
