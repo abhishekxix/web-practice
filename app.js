bec4e3d61a543e0c921b9e444aed9ff2ccf8f44c
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-10-28 23:38:12 +0530

    XHR

--- app.js
+++ app.js
@@ -1,33 +1,19 @@
-const heading1 = document.querySelector('.one');
-const heading2 = document.querySelector('.two');
-const heading3 = document.querySelector('.three');
+const xhr = new XMLHttpRequest();
 
-const btn = document.querySelector('.btn');
+xhr.open('GET', './api/sample.txt');
 
-btn.addEventListener('click', async () => {
-  console.log('hello');
-  try {
-    await addColor(1000, heading1, 'red');
-    await addColor(1000, heading2, 'green');
-    await addColor(1000, heading3, 'blue');
-  } catch (err) {
-    console.error(err);
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    console.log(xhr.responseText);
+    const text = document.createElement('p');
+    text.textContent = xhr.responseText;
+    document.body.appendChild(text);
+  } else {
+    console.log({
+      status: xhr.status,
+      text: xhr.statusText,
+    });
   }
-});
-// async functions return a promise by default
-async function dipslayColor() {
-  // a(); //promise is rejected
-  return 'hello'; // the return value is wrapped inside of a promise
-}
-console.log(dipslayColor());
+};
 
-function addColor(time, element, color) {
-  return new Promise((resolve, reject) => {
-    if (!element) reject();
-
-    setTimeout(() => {
-      element.style.color = color;
-      resolve();
-    }, time);
-  });
-}
+xhr.send();
