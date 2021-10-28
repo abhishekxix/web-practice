3ba7dc8be68b3d8d7940cbf1ce91473f1a146784
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-10-28 20:01:57 +0530

    modules intoduction

---
+++ utils/data.js
@@ -0,0 +1,10 @@
+export const random = 'random'; //named export
+
+export const people = [
+  { name: 'john', job: 'developer' },
+  { name: 'susan', job: 'designer' },
+  { name: 'anna', job: 'boss' },
+];
+
+// default export
+export default random;
